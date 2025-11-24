a13a74af6ff04c7c11cebd1373ba212ea98d5bd6
Degefa Lemma <dagefalemma@gmail.com>
2025-11-24 12:30:12 +0300

    My First laravel modification on the view

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return "This is to test the push on git repository";
 });
 
 require __DIR__.'/auth.php';
